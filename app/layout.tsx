8d99e547c158b8e591fdb12146a488dba66cc80c
Anuj Kumar <anuj9654949185@gmail.com>
2024-07-30 00:10:19 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import 'react-datepicker/dist/react-datepicker.css'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Neom",
+  title: "MeetUp",
   description: "Video conferencing app",
   icons:{
     icon: '/icons/logo.svg'
