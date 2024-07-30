6bc242f23c84c39708307f71cbc6005c5a5f3a78
Anuj Kumar <anuj9654949185@gmail.com>
2024-07-30 22:44:15 +0530

    update

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -37,7 +37,7 @@ const MobileNav = () => {
                     alt='Neom logo'
                     className='max-sm:size-10'
                 />
-                <p className='text-[26px] font-extrabold text-white '>Neom</p>
+                <p className='text-[26px] font-extrabold text-white '>MeetUp</p>
             </Link>
 
             <div className='flex h-[calc(100vh-72px)] 
